24b6ce146c351087ecd9e4cd5cd1ae172c0a289b
Praveen Raj <rajp03262@gmail.com>
2023-12-25 12:22:06 +0530

    add a middle ware and a function to handlle error

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    error: message,
+    statusCode,
+  });
+});
