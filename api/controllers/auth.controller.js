c66976deda2ab92337ea02507f0803c4d175a6cd
Praveen Raj <rajp03262@gmail.com>
2023-12-25 16:35:06 +0530

    comolete signup page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json({ message: "user created successfully" });
+    res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
     next(error);
   }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"; // Import the cors package
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 dotenv.config();
@@ -16,21 +17,25 @@ mongoose
 
 const app = express();
 
+// Enable CORS for all routes
+app.use(cors());
+
 app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.use("/api/user", userRoutes);
-app.use("/api/auth", authRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
-    error: message,
+    message,
     statusCode,
   });
 });
+
